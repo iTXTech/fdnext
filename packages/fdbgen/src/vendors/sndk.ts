eca7c5f39dd9188f13295d69bf16d6c4eba3c4e7
PeratX <peratx@itxtech.org>
2026-05-07 20:04:54 +0800

    feat: update SNDK part number decoding

--- packages/fdbgen/src/vendors/sndk.ts
+++ packages/fdbgen/src/vendors/sndk.ts
@@ -1,7 +1,12 @@
 import type { VendorDecoder } from "./types";
 
+const SNDK_TWELVE_DIGIT_MARKING = /^[0-9]{4}[0-9A-Z][HKRPQVXEFCJGU][0-9A-Z]{6}$/;
+
 export const sndkVendor: VendorDecoder = {
   id: "sndk",
   aliases: ["sandisk", "san disk", "westerndigital", "western digital", "wd"],
-  identify: (partNumber) => /^(SD|S34|S35|SANDISK|SNDK|DFT|MDT|05[0-9]{3})/.test(partNumber)
+  identify: (partNumber) => {
+    const normalized = partNumber.toUpperCase();
+    return /^(SD|S34|S35|SANDISK|SNDK|DFT|MDT|05[0-9]{3})/.test(normalized) || SNDK_TWELVE_DIGIT_MARKING.test(normalized);
+  }
 };
